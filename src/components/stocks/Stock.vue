6fd9bf53f4670f916a13a77e5c30f886d786bbf7
Jannes Brunner <jannes.brunner@oo-services.com>
2020-10-02 11:47:30 +0200

    Populate Stock Data + buy check

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,8 +2,8 @@
     <div class="col-sm6 col-md-4">
         <div class="panel panel-success">
             <div class="pane-heading">
-                <h3 class="panel-title">NAME
-                    <small>price: Price</small>
+                <h3 class="panel-title">{{ stock.name }}
+                    <small>price: {{ stock.price }}</small>
                 </h3>
             </div>
             <div class="panel-body">
@@ -12,12 +12,34 @@
                     type="number"
                      class="form-control"
                      placeholder="Quantity"
+                     v-model="quantity"
                      > 
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success">Buy</button>
+                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(Number(quantity))">Buy</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0,
+        }
+    },
+    methods: {
+        buyStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log(order);
+            this.quantity = 0;
+        }
+    }
+}
+</script>
