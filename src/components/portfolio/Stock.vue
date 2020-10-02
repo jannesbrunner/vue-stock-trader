6ebc73a7c1520a11a55311a4aceeb00bc53b1db8
Jannes Brunner <jannes.brunner@oo-services.com>
2020-10-02 14:46:58 +0200

    Add portfolio stock component

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="col-sm6 col-md-4">
+        <div class="panel panel-success">
+            <div class="pane-heading">
+                <h3 class="panel-title">{{ stock.name }}
+                    <small>price: {{ stock.price }} | Quantity: {{ stock.quantity }}</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                    type="number"
+                     class="form-control"
+                     placeholder="Quantity"
+                     v-model="quantity"
+                     > 
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(Number(quantity))">Sell</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+<script>
+import {mapActions} from 'vuex';
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0,
+        }
+    },
+    methods: {
+        ...mapActions([
+            'sellStock'
+        ]),
+        sellStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            this.sellStock()
+        }
+    }
+}
+</script>
