21e8d96fca8611bdcaf990c3bb84a4a01c4b0cc7
Jannes Brunner <jannes.brunner@oo-services.com>
2020-10-02 16:35:18 +0200

    Setup vue-resource

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import VueRouter from 'vue-router';
 
 import { routes } from './routes';
 import store from './store/store';
+import VueResource from 'vue-resource';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'localhost';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
