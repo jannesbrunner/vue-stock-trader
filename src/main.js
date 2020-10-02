d431d6a0f6da0f219b5e9addfc0c2a34c7bdc861
Jannes Brunner <jannes.brunner@oo-services.com>
2020-10-02 11:16:16 +0200

    Setup Vue Router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
+
+/*
+<router-link class="nav-item" to="/stocks" tag="li" activeClass="active"
+          ><a class="nav-link">Stocks</a></router-link */
